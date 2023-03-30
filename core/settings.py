f322ffe33c276c12c9656e249f470bb15f8dc6b0
Atharva <asphadnis1@gmail.com>
2023-03-30 13:48:19 +0530

    Add env variables

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n+c3gc13ul&pv+mtfvcwv-@fd=+$^e+1$94+y!i9v@*#f33yyc'
+# SECRET_KEY = 'django-insecure-n+c3gc13ul&pv+mtfvcwv-@fd=+$^e+1$94+y!i9v@*#f33yyc'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-n+c3gc13ul&pv+mtfvcwv-@fd=+$^e+1$94+y!i9v@*#f33yyc')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
